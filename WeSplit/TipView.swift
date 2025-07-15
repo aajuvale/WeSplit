30c7f68cd770664e218e9d4cb81a156d0f8e4f63
aajuvale <ahmed.juvale@gmail.com>
2025-07-15 16:22:28 -0700

    feat: implemented tip view

---
+++ WeSplit/TipView.swift
@@ -0,0 +1,39 @@
+//
+//  TipView.swift
+//  WeSplit
+//
+//  Created by Ahmed Juvale on 7/15/25.
+//
+
+import SwiftUI
+
+struct TipView: View {
+    
+    @State
+    private var checkAmount = 0.0
+    
+    @State
+    private var numberOfPeople = 2
+    
+    @State
+    private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section {
+                Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+            }
+        }
+    }
+}
+
+#Preview {
+    TipView()
+}
