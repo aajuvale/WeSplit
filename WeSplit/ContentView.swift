e7e8362f327277dae5747e7bebedafad3bac10d0
aajuvale <ahmed.juvale@gmail.com>
2025-07-15 16:39:12 -0700

    feat: implemented done button to dismiss keyboard, using Focus State

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,24 +9,67 @@ import SwiftUI
 
 struct ContentView: View {
     
-    // SwiftUI needs to be able to read and write value so $ is needed for two way binding, tells swift to read and write value automatically
-    
-    let students = ["Scott", "Kurt", "Logan"]
     @State
-    private var selectedStudent = "Logan"
+    private var checkAmount = 0.0
+    
+    @State
+    private var numberOfPeople = 2
+    
+    @State
+    private var tipPercentage = 20
+    
+    @FocusState
+    private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
     
     var body: some View {
-        
-        // WeSplit 7/11
         NavigationStack {
             Form {
-                Picker("Select your Student: ", selection: $selectedStudent){
-                    ForEach(students, id: \.self){
-                        Text($0)
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section("How much do you want to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
                     }
                 }
             }
-            .navigationTitle("Select a Student")
         }
     }
 }
